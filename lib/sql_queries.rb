6f762fa8c6bd47282201c8bc436df21ea7e27ec9
Blen Wondimagegn <mekuriablen@gmail.com>
2020-09-16 23:41:48 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -42,7 +42,6 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
   ON "
   
 >>>>>>> 0ca993c7fcf1b6794fd3211b501bc3876142f5cb
-  
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
